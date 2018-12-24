0380dd3377d5c42821fad62d6bc7153cec701d46
Kaka <kaka.xie@mrobot.cn>
2018-12-24 17:07:34 +0800

    fix get_my_id bug

--- Modules/id.c
+++ Modules/id.c
@@ -10,14 +10,14 @@ void id_key_init(void)
 {
     GPIO_InitTypeDef  GPIO_InitStructure;
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);   //使能端口时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);   //使能端口时钟
 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8;  //端口配置
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;   //input
 //    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; //IO口速度为50MHz
     GPIO_Init(GPIOB, &GPIO_InitStructure);  //根据设定参数初始化GPIO
 
-//    GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
 
 }
 
@@ -70,7 +70,7 @@ uint8_t get_my_id(void)
         cnt++;
     }
 
-    if((new_key_value != 0) && (new_key_value <= 0x1f))
+    if((new_key_value != 0) && (new_key_value <= 0x3f))
     {
         return new_key_value;
     }
